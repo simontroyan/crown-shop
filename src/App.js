af01fc4fb03953f5245800c4f7a9faf36750253b
Saymon Troyan <SaymonTR@Saymons-MacBook-Pro.local>
2020-05-11 16:25:02 +0200

    Change

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Homepage from './homepage.component'
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      hello world
+    </div>
+  );
+}
+
+export default App;
