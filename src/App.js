1e9c086dee252cd4fdea80c012957604eeec8db9
Saymon Troyan <SaymonTR@Saymons-MacBook-Pro.local>
2020-05-12 21:19:32 +0200

    Created ShopPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import Homepage from './pages/homepage/homepage.component'
-
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>HOME PAGE</h1>
-    </div>
-  )
-}
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Directory from '../../components/directory/directory.component'
-import './homepage.styles.scss'
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
 
-const Homepage = () => (
-    <div className="homepage">
-        <Directory/>
-    </div>
-)
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
 
-export default Homepage
+export default HomePage;
